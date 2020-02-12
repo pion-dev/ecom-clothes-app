97832639f10cf97bcdad6c5cae066e7a3de0d46a
pion-dev <fouad.liady@beije.it>
2020-02-12 13:00:07 +0100

    updating our app with a shop page collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepages/homepage.component';
+import {Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App(){
 
   return (
   <div>
-  <HomePage></HomePage>
+  <Switch>
+  <Route exact path='/' component={HomePage}></Route>
+  <Route   path='/shop' component={ShopPage}></Route>
+  </Switch>
   </div>);
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,7 +49,8 @@ class Directory extends React.Component{
     render(){
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => (<MenuItem key={id} title={title} imageUrl={imageUrl} size = {size}></MenuItem>))}
+                {this.state.sections.map(({ id, ...othersSectionProps}) =>
+                 (<MenuItem key={id} {...othersSectionProps}></MenuItem>))}
                 
             </div>
         )
